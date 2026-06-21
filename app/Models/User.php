0d77ee0b5c993d043a4e5042c2508bae1afbec7e
neet91 <harithtajudin91@gmail.com>
2026-06-21 12:07:50 +0800

    add user apitoken

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
-
+use Laravel\Sanctum\HasApiTokens;
 /**
  * @property int $id
  * @property string $name
