be24585f1258f9569b574a94fa269788db44236c
neet91 <harithtajudin91@gmail.com>
2026-06-21 12:16:55 +0800

    return json

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Resources\CompanyResource;
+use App\Models\CompanyAlpine;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/companies/{company}', function (CompanyAlpine $company) {
+    return new CompanyResource(
+        $company->load('employees')->loadCount('employees')
+    );
+});
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
